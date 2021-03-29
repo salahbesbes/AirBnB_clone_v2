9a1fedb99202084be91e89af235631c290fc1f5f
salahbesbes <salah_besbes@ymail.com>
2021-03-29 15:03:00 +0200

    task 6

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,8 +1,7 @@
-#!/usr/bin/python3
-
+-- create hbnb_test_db DB
 CREATE DATABASE IF NOT EXISTS  hbnb_test_db;
 USE hbnb_test_db;
 CREATE USER IF NOT EXISTS hbnb_test@localhost IDENTIFIED BY 'hbnb_test_pwd';
 GRANT SELECT ON performance_schema.* TO hbnb_test@localhost;
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO hbnb_test@localhost;
-FLUSH PRIVILEGES;
+
