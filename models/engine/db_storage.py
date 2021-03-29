3178a2926672c2bc98d54405065fc7eda64b480b
salahbesbes <salah_besbes@ymail.com>
2021-03-29 15:51:22 +0200

    task 6 done

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """ DB modules """
 import os
 from models.base_model import BaseModel, Base
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,7 +10,6 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        from models.base_model import BaseModel
         if cls:
             try:
                 new_obj = {}
@@ -30,9 +29,9 @@ class FileStorage:
 
     def save(self):
         """Saves storage dictionary to file"""
-        with open(FileStorage.__file_path, 'w') as f:
+        with open(self.__file_path, 'w') as f:
             temp = {}
-            temp.update(FileStorage.__objects)
+            temp.update(self.__objects)
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f, indent=2)
