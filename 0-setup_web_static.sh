3063c38d390f8e1ac38b16852c11cce6bd7cd41d
salahbesbes <salah_besbes@ymail.com>
2021-04-15 16:13:05 +0200

    prepare for deploy

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,23 +1,27 @@
 #!/usr/bin/env bash
 # preparing for deploying
-sudo mkdir -p /data
+
 sudo mkdir -p /data/web_static/
 sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 
-echo "
-<html>
+echo \
+"<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>
-" > /data/web_static/releases/test/index.html
+</html>" > /data/web_static/releases/test/index.html
 
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R "$USER":"$USER" /data/
-newroot="/data/web_static/current"
+# newroot='root \/data\/web_static\/current;'
+pattern='root \/var\/www\/html;'
+path="/etc/nginx/sites-enabled/default"
 
-sudo sed  "s/root \/var\/www\/html;/ $newroot/g" /etc/nginx/sites-available/default
+newlocation="\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n"
+
+pattern="root \/var\/www\/html;"
+sed -ie "/$pattern/a\ $newlocation" $path
