9baafe27527beb45d462c9f5966d5cd536050e5a
salahbesbes <salah_besbes@ymail.com>
2021-04-16 23:22:18 +0200

    unzip|tar archive and deploy static version

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ fabric module """
+from os import path
 from fabric.api import *
-import re
 
 env.hosts = ['34.75.197.138', '104.196.173.55']
 
@@ -12,33 +12,27 @@ def do_deploy(archive_path):
     Args:
         archive_path ([string]): [path to the archive]
     """
-    if not bool(path.exists(archive_path)):
+    if path.exists(archive_path) is False:
         return False
+    # pattern = r'web_static_+\d+\.tgz$'
+    # archive_name = re.search(pattern, archive_path).group()
+    archive_name = archive_path.split('/')[1]
+    archive = archive_name.split('.')[0]
+    release = '/data/web_static/releases'
+    archive_destination = '/data/web_static/releases/{}'.format(
+        archive)
+    put(archive_path, '/tmp')
+    run('mkdir -p {dest} '.format(dest=archive_destination))
+    run('tar -xzf /tmp/{ar_name} -C {dest}'
+        .format(ar_name=archive_name, dest=archive_destination))
+    run('rm -f /tmp/{ar_name} '.format(ar_name=archive_name))
 
-    else:
-        pattern = r'web_static_+\d+\.tgz$'
-        try:
-            archive_name = re.search(pattern, archive_path).group()
-            archive = archive_name.split('.')[0]
-            archive_destination = '/data/web_static/releases/{}'.format(
-                archive)
-            put(archive_path, '/tmp')
-            run('mkdir -p {dest} '.format(dest=archive_destination))
-            run('tar -xzf /tmp/{ar_name} -C {dest} '
-                .format(ar_name=archive_name, dest=archive_destination))
-            run('rm -f /tmp/{ar_name} '.format(ar_name=archive_name))
+    run('mv {release}/web_static/* {release}/{archive}/'
+        .format(release=release, archive=archive))
 
-            run('mv /data/web_static/releases/{archive}/web_static/* '
-                .format(archive=archive) +
-                '/data/web_static/releases/{archive}/'
-                .format(archive=archive))
-
-            run('rm -rf /data/web_static/releases/' +
-                '{archive}/web_static'.format(archive=archive))
-            run('rm -rf /data/web_static/current ')
-            run('ln -sf /data/web_static/releases/{archive} '
-                .format(archive=archive) + ' /data/web_static/current')
-            return True
-
-        except Exception:
-            return False
+    run('rm -rf {release}/{archive}/web_static'
+        .format(release=release, archive=archive))
+    run('rm -rf /data/web_static/current')
+    run('ln -s {release}/{archive}/data/web_static/current'
+        .format(release=release, archive=archive))
+    return True
