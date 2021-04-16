64a9f1d6b29dc9e3a89e0fab45138ba6437a515e
salahbesbes <salah_besbes@ymail.com>
2021-04-16 15:36:24 +0200

    dd

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -12,11 +12,11 @@ sudo chown -R ubuntu:ubuntu /data/
 echo "this is salah besbes" > /data/web_static/releases/test/index.html
 
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-# newroot='root \/data\/web_static\/current;'
-# pattern='root \/var\/www\/html;'
 # path="/etc/nginx/sites-available/default"
 
-# newlocation="\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n"
-# sed -ie "/$pattern/a\ $newlocation" $path
+# newlocation="\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n"
 
+# pattern="root \/var\/www\/html;"
+
+# sed -ie "/$pattern/a\ $newlocation" $path
 sudo service nginx restart
