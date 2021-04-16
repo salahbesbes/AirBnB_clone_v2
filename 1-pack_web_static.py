f964a0c1afe2e45b8ee5a00415227be6d1e41e17
salahbesbes <salah_besbes@ymail.com>
2021-04-16 18:28:07 +0200

    compress web_static using fabric module

---
+++ 1-pack_web_static.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" fabric module """
+from fabric.api import local
+import time
+
+
+def do_pack():
+    """Make a tar.gz archive of da (/web_static )."""
+    name_folder = "web_static"
+    archive_name = "{}_{}.tgz".format(name_folder, time.strftime(
+        "%Y%m%d%H%M%S", time.gmtime()))
+    path_archive = './versions/{}'.format(archive_name)
+    local("mkdir -p versions; \
+            tar -zvcf {} {}".format(path_archive, name_folder))
