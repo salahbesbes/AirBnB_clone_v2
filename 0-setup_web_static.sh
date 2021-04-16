5b068b6566ab3b9603b0066acd4b7df59da0422f
salahbesbes <salah_besbes@ymail.com>
2021-04-16 15:46:47 +0200

    dd

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,16 +7,16 @@ sudo apt-get install -y nginx
 
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/ 
-sudo chown -R ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data
 
 echo "this is salah besbes" > /data/web_static/releases/test/index.html
 
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-# path="/etc/nginx/sites-available/default"
+path="/etc/nginx/sites-available/default"
 
-# newlocation="\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n"
+newlocation="\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n"
 
-# pattern="root \/var\/www\/html;"
+pattern="root \/var\/www\/html;"
 
-# sed -ie "/$pattern/a\ $newlocation" $path
+sed -ie "/$pattern/a\ $newlocation" $path
 sudo service nginx restart
