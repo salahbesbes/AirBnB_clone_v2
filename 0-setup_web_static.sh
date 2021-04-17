4a930fc3ba7207f39d6b9bad1a854b51436c1a3a
salahbesbes <salah_besbes@ymail.com>
2021-04-17 16:59:35 +0200

    prepare server to deploy

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -10,7 +10,8 @@ echo "Hello AirBnb" | sudo tee /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test /data/web_static/current 
 chown ubuntu:ubuntu -hR /data/
 
-path='\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n'
+path='/etc/nginx/sites-available/default'
+newlocation='\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n'
 pattern='index index.html index.htm index.nginx-debian.html;'
 sudo sed -i "/$pattern/ a \ $newlocation" $path
 sudo service nginx restart
