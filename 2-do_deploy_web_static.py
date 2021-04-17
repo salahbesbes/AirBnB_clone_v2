24e6c266c0e9ed264b8eae405e46a197f24ed19b
salahbesbes <salah_besbes@ymail.com>
2021-04-17 19:19:14 +0200

    deploy an archive in its own place

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,28 +2,28 @@
 """ fabric module """
 from fabric.api import run, env, put
 from os import path
+import re
 
 env.hosts = ['34.75.197.138', '104.196.173.55']
+env.user = 'ubuntu'
 
 
 def do_deploy(archive_path):
     """ Deploy archive
-
+        line to call: fab -f 2-do_deploy_web_static.py do_deploy:archive_path=\
+            versions/web_static_20170315003959.tgz -u ubuntu(optional)
     Args:
         archive_path ([string]): [path to the archive]
     """
     if path.exists(archive_path) is False:
         return False
-    # pattern = r'web_static_+\d+\.tgz$'
-    # archive_name = re.search(pattern, archive_path).group()
-    archive_name = archive_path.split('/')[1]
+    pattern = r'web_static_+\d+\.tgz$'
+    archive_name = re.search(pattern, archive_path).group()
     archive = archive_name.split('.')[0]
     release = '/data/web_static/releases'
     archive_destination = '/data/web_static/releases/{archive}/'.format(
         archive=archive)
     put(archive_path, '/tmp')
-    run('rm -fr {release}/{archive}/'
-        .format(release=release, archive=archive))
     run('mkdir -p {dest} '.format(dest=archive_destination))
     run('tar -xzf /tmp/{ar_name} -C {dest}'
         .format(ar_name=archive_name, dest=archive_destination))
