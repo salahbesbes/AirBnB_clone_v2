bed83d0a07978256709687e8d5ddd2ced90182a2
salahbesbes <salah_besbes@ymail.com>
2021-04-17 16:38:06 +0200

    prepare server to deploy

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,23 +2,19 @@
 # preparing for deploying
 
 sudo apt-get -y update
-sudo apt-get -y upgrade
 sudo apt-get install -y nginx
 
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/ 
-echo "Salah besbes page" > /data/web_static/releases/test/index.html
+echo "Salah besbes page" | sudo tee /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test /data/web_static/current 
 
 chown ubuntu:ubuntu -R /data/
-path="/etc/nginx/sites-enabled/default"
-
-newlocation="\tlocation /hbnb_static \{\
-\n\t\talias /data/web_static/current\;\n\
-autoindex off\;\n\
-\t\}\n"
-
-pattern="root \/var\/www\/html;"
-
-sed -ie "/$pattern/a\ $newlocation" $path
+path="/etc/nginx/sites-available/default"
+newlocation='\tlocation /hbnb_static/ {\
+\t\talias /data/web_static/current/;\
+\t}\
+'
+pattern='root \/var\/www\/html;'
+sudo sed "/$pattern/ a \ $newlocation" $path
 sudo service nginx restart
