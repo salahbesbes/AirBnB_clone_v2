c35b8bbc0f36d0152d5b9ce95863373defbf3961
salahbesbes <salah_besbes@ymail.com>
2021-04-17 19:15:53 +0200

    deploy an archive

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,9 +6,13 @@ import time
 
 def do_pack():
     """Make a tar.gz archive of da (/web_static )."""
-    name_folder = "web_static"
-    archive_name = "{}_{}.tgz".format(name_folder, time.strftime(
-        "%Y%m%d%H%M%S", time.gmtime()))
-    path_archive = './versions/{}'.format(archive_name)
-    local("mkdir -p versions; \
-            tar -zvcf {} {}".format(path_archive, name_folder))
+    try:
+        name_folder = "web_static"
+        archive_name = "{}_{}.tgz".format(name_folder, time.strftime(
+            "%Y%m%d%H%M%S", time.gmtime()))
+        path_archive = './versions/{}'.format(archive_name)
+        local("mkdir -p versions; \
+                tar -zvcf {} {}".format(path_archive, name_folder))
+        return path_archive
+    except Exception:
+        return None
